8d845f055348ced27700ee8b011dcae2984eb53e
Victor Flores Laptop <victor_2002fj@hotmail.com>
2024-12-04 13:32:36 -0600

    Terminado contenido faltan imagenes

--- build/js/app.js
+++ build/js/app.js
@@ -1,2 +1,2 @@
-function toggleContent(t){document.getElementById(t).classList.toggle("oculto")}
+function toggleContent(t){const e=document.getElementById(t);e.classList.contains("activo")?e.classList.remove("activo"):(document.querySelectorAll(".modulo_desplegar.activo").forEach(t=>{t.classList.remove("activo")}),e.classList.add("activo"))}document.addEventListener("DOMContentLoaded",()=>{document.body.classList.add("loaded")});
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,17 @@
 function toggleContent(id) {
-    const content = document.getElementById(id);
-    content.classList.toggle('oculto');
-  }  
+  const content = document.getElementById(id);
+
+  if (content.classList.contains('activo')) {
+    content.classList.remove('activo');
+  } else {
+    document.querySelectorAll('.modulo_desplegar.activo').forEach((openContent) => {
+      openContent.classList.remove('activo');
+    });
+
+    content.classList.add('activo');
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.classList.add("loaded");
+});
